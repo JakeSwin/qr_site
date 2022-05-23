a876b5b245b3158b09d7bc5dfd60bba83b06b270
Jacob Swindell <56450014+JakeSwin@users.noreply.github.com>
2022-05-23 16:06:06 +0100

    add dynamic hash URL to QR code

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const date = new Date()
+const hour = date.getHours()
+
+if (hour < 6) {
+    date.setDate(date.getDate() - 1)
+}
+
+const day = date.getDate()
+const month = date.getMonth()
+
+const fulldate = day.toString() + month.toString() + hour.toString()
+
+const todayhash = CryptoJS.SHA1(fulldate + "clank").toString()
+
+const qrlink = "https://radiant-forest-06530.herokuapp.com/qrsignin/" + todayhash
+
 var qrcode = new QRCode(document.querySelector(".qrcode"), {
-  text: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
-  width: 256,
-  height: 256,
+  text: qrlink,
+  width: 400,
+  height: 400,
   colorDark: "#000000",
   colorLight: "#ffffff",
   correctLevel: QRCode.CorrectLevel.H
